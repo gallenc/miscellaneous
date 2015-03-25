07487a199e6b2d78bedcffd3a2193e6e810e494c
cgallen <cgallen@opennms.org>
2015-03-25 22:06:36 +0000

    working wmi - need password

--- TMFCatalyst2015/opennms-config/quick_deploy.sh
+++ TMFCatalyst2015/opennms-config/quick_deploy.sh
@@ -10,26 +10,26 @@ DATE=$(date +"%Y%m%d%H%M")
 
 ## move /etc config files
 
-# mv /opt/opennms/etc/collectd-configuration.xml /opt/opennms/etc/collectd-configuration.xml.$DATE
-# cp ./collectd-configuration.xml /opt/opennms/etc/
+ mv /opt/opennms/etc/collectd-configuration.xml /opt/opennms/etc/collectd-configuration.xml.$DATE
+ cp ./collectd-configuration.xml /opt/opennms/etc/
 
-# mv /opt/opennms/etc/datacollection-config.xml /opt/opennms/etc/datacollection-config.xml$DATE
-# cp ./datacollection-config.xml /opt/opennms/etc/
+ mv /opt/opennms/etc/datacollection-config.xml /opt/opennms/etc/datacollection-config.xml.$DATE
+ cp ./datacollection-config.xml /opt/opennms/etc/
 
-# mv /opt/opennms/etc/wmi-config.xml /opt/opennms/etc/wmi-config.xml$DATE
-# cp ./wmi-config.xml /opt/opennms/etc/
+ mv /opt/opennms/etc/wmi-config.xml /opt/opennms/etc/wmi-config.xml$DATE
+ cp ./wmi-config.xml /opt/opennms/etc/
 
-# mv /opt/opennms/etc/wmi-datacollection-config.xml /opt/opennms/etc/wmi-datacollection-config.xml$DATE
-# cp ./wmi-datacollection-config.xml /opt/opennms/etc/
+ mv /opt/opennms/etc/wmi-datacollection-config.xml /opt/opennms/etc/wmi-datacollection-config.xml.$DATE
+ cp ./wmi-datacollection-config.xml /opt/opennms/etc/
 
 
 ## move graph properties files
 
-# mv /opt/opennms/etc/snmp-graph.properties.d/microsoft-wmi-graph.properties   /opt/opennms/etc/snmp-graph.properties.d/microsoft-wmi-graph.properties$DATE
-# cp ./microsoft-wmi-graph.properties /opt/opennms/etc/snmp-graph.properties.d/
+ mv /opt/opennms/etc/snmp-graph.properties.d/microsoft-wmi-graph.properties   /opt/opennms/etc/snmp-graph.properties.d/microsoft-wmi-graph.properties.$DATE
+ cp ./microsoft-wmi-graph.properties /opt/opennms/etc/snmp-graph.properties.d/
 
-# mv /opt/opennms/etc/snmp-graph.properties.d/wmi-memory-graph.propertiess   /opt/opennms/etc/snmp-graph.properties.d/wmi-memory-graph.properties$DATE
-# cp ./wmi-memory-graph.properties /opt/opennms/etc/snmp-graph.properties.d/
+ mv /opt/opennms/etc/snmp-graph.properties.d/wmi-memory-graph.properties   /opt/opennms/etc/snmp-graph.properties.d/wmi-memory-graph.properties.$DATE
+ cp ./wmi-memory-graph.properties /opt/opennms/etc/snmp-graph.properties.d/
 
-# mv /opt/opennms/etc/snmp-graph.properties.d/wmi-physdisk-graph.properties   /opt/opennms/etc/snmp-graph.properties.d/wmi-physdisk-graph.properties$DATE
-# cp ./wmi-physdisk-graph.properties /opt/opennms/etc/snmp-graph.properties.d/
+ mv /opt/opennms/etc/snmp-graph.properties.d/wmi-physdisk-graph.properties   /opt/opennms/etc/snmp-graph.properties.d/wmi-physdisk-graph.properties.$DATE
+ cp ./wmi-physdisk-graph.properties /opt/opennms/etc/snmp-graph.properties.d/
